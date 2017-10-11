56456b4990c400af3515fe554246096f8c928105
Moto0116 <moto01160902@gmail.com>
2017-10-11 13:55:13 +0900

    Object2DBaseクラス追加

---
+++ Chemical/Chemical/Main/Object2DBase/Object2DBase.cpp
@@ -0,0 +1,112 @@
+﻿/**
+ * @file   Object2DBase.cpp
+ * @brief  Object2DBaseクラスの実装
+ * @author morimoto
+ */
+
+//----------------------------------------------------------------------
+// Include
+//----------------------------------------------------------------------
+#include "Object2DBase.h"
+
+#include "Debugger\Debugger.h"
+#include "DirectX11\GraphicsDevice\Dx11GraphicsDevice.h"
+#include "DirectX11\TextureManager\Dx11TextureManager.h"
+
+
+//----------------------------------------------------------------------
+// Constructor	Destructor
+//----------------------------------------------------------------------
+Object2DBase::Object2DBase() :
+	m_pVertex(nullptr),
+	m_Pos(D3DXVECTOR2(0, 0)),
+	m_Size(D3DXVECTOR2(0, 0))
+{
+	m_pDrawTask = new Lib::Draw2DTask();
+	m_pUpdateTask = new Lib::UpdateTask();
+
+	m_pDrawTask->SetObject(this);
+	m_pUpdateTask->SetObject(this);
+}
+
+Object2DBase::~Object2DBase()
+{
+	delete m_pUpdateTask;
+	delete m_pDrawTask;
+}
+
+
+//----------------------------------------------------------------------
+// Public Functions
+//----------------------------------------------------------------------
+bool Object2DBase::Initialize()
+{
+	return true;
+}
+
+void Object2DBase::Finalize()
+{
+}
+
+void Object2DBase::UpdateStartUp()
+{
+}
+
+void Object2DBase::Update()
+{
+}
+
+void Object2DBase::DrawStartUp()
+{
+}
+
+void Object2DBase::Draw()
+{
+	m_pVertex->ShaderSetup();
+	m_pVertex->WriteConstantBuffer(&m_Pos);
+	m_pVertex->Draw();
+}
+
+
+//----------------------------------------------------------------------
+// Protected Functions
+//----------------------------------------------------------------------
+bool Object2DBase::CreateVertex2D()
+{
+	m_pVertex = new Lib::Dx11::Vertex2D();
+	if (!m_pVertex->Initialize(SINGLETON_INSTANCE(Lib::Dx11::GraphicsDevice)))
+	{
+		OutputErrorLog("2D描画オブジェクトの初期化に失敗しました");
+		SafeDelete(m_pVertex);
+		return false;
+	}
+
+	if (!m_pVertex->CreateVertexBuffer(&m_Size))
+	{
+		OutputErrorLog("頂点バッファの生成に失敗しました");
+		m_pVertex->Finalize();
+		SafeDelete(m_pVertex);
+		return false;
+	}
+
+	if (!m_pVertex->WriteConstantBuffer(&m_Pos))
+	{
+		OutputErrorLog("定数バッファへの書き込みに失敗しました");
+		m_pVertex->ReleaseVertexBuffer();
+		m_pVertex->Finalize();
+		SafeDelete(m_pVertex);
+		return false;
+	}
+
+	return true;
+}
+
+void Object2DBase::ReleaseVertex2D()
+{
+	if (m_pVertex != nullptr)
+	{
+		m_pVertex->ReleaseVertexBuffer();
+		m_pVertex->Finalize();
+		SafeDelete(m_pVertex);
+	}
+}
---
+++ Chemical/Chemical/Main/Object2DBase/Object2DBase.h
@@ -0,0 +1,89 @@
+﻿/**
+ * @file   Object2DBase.cpp
+ * @brief  Object2DBaseクラスの実装
+ * @author morimoto
+ */
+#ifndef OBJECT2DBASE_H
+#define OBJECT2DBASE_H
+
+//----------------------------------------------------------------------
+// Include
+//----------------------------------------------------------------------
+#include "ObjectManagerBase\ObjectBase\ObjectBase.h"
+#include "DirectX11\Vertex2D\Dx11Vertex2D.h"
+#include "TaskManager\TaskBase\DrawTask\DrawTask.h"
+#include "TaskManager\TaskBase\UpdateTask\UpdateTask.h"
+
+
+/**
+ * 2Dオブジェクトの基底クラス
+ */
+class Object2DBase : public Lib::ObjectBase
+{
+public:
+	/**
+	 * コンストラクタ
+	 */
+	Object2DBase();
+
+	/**
+	 * デストラクタ
+	 */
+	virtual ~Object2DBase();
+
+	/**
+	 * 初期化処理
+	 * @return 初期化に成功したらtrue 失敗したらfalse
+	 */
+	virtual bool Initialize();
+
+	/**
+	 * 終了処理
+	 */
+	virtual void Finalize();
+
+	/**
+	 * オブジェクトの更新前処理
+	 */
+	virtual void UpdateStartUp();
+
+	/**
+	 * オブジェクトの更新
+	 */
+	virtual void Update();
+
+	/**
+	 * オブジェクトの描画前処理
+	 */
+	virtual void DrawStartUp();
+
+	/**
+	 * オブジェクトの描画
+	 */
+	virtual void Draw();
+
+protected:
+	/**
+	 * 2D描画オブジェクトの生成
+	 * @return 生成に成功したらtrue 失敗したらfalse
+	 */
+	bool CreateVertex2D();
+
+	/**
+	 * 2D描画オブジェクトの破棄
+	 */
+	void ReleaseVertex2D();
+
+
+	Lib::Draw2DTask*	m_pDrawTask;	//!< 描画タスクオブジェクト.
+	Lib::UpdateTask*	m_pUpdateTask;	//!< 更新タスクオブジェクト.
+
+	Lib::Dx11::Vertex2D*	m_pVertex;		//!< 2D描画オブジェクト.
+	D3DXVECTOR2				m_Pos;			//!< 描画座標.
+	D3DXVECTOR2				m_Size;			//!< 描画サイズ.
+	int						m_TextureIndex;	//!< テクスチャのインデックス.
+
+};
+
+
+#endif // !OBJECT2DBASE_H
