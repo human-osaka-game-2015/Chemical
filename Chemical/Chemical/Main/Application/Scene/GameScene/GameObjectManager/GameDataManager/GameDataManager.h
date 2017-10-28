f1f40105af3de8c8d0ab3f6e2602dcb25969d9bd
Keitaro0408 <kei.kotani77@gmail.com>
2017-10-28 20:15:53 +0900

    GameDataManagerの実装
    - ゲームのデータを管理するシングルトンクラスを実装

---
+++ Chemical/Chemical/Main/Application/Scene/GameScene/GameObjectManager/GameDataManager/GameDataManager.h
@@ -0,0 +1,48 @@
+﻿/**
+ * @file   GameDataManager.h
+ * @brief  GameDataManagerクラスのヘッダファイル
+ * @author kotani
+ */
+#ifndef GAME_GAMEDATA_MANAGER_H
+#define GAME_GAMEDATA_MANAGER_H
+#include "SingletonBase\SingletonBase.h"
+#include <D3DX10.h>
+
+namespace Game
+{
+	class GameDataManager : public Lib::SingletonBase<GameDataManager>
+	{
+		friend Lib::SingletonBase<GameDataManager>;
+	public:
+		/**
+		 * プレイヤーの座標のポインタをセットする 
+		 * @param[in] プレイヤー座標のポインタ
+		 */
+		void SetPlayerPosPtr(D3DXVECTOR2* _pPlayerPos)
+		{
+			m_PlayerPos = _pPlayerPos;
+		}
+
+		/**
+		 * プレイヤー座標を取得する  
+		 * @return プレイヤー座標
+		 */
+		D3DXVECTOR2 GetPlayerPos()
+		{
+			return *m_PlayerPos;
+		}
+
+	private:
+		/*** コンストラクタ */
+		GameDataManager();
+
+		/*** デストラクタ */
+		virtual ~GameDataManager();
+	
+		D3DXVECTOR2* m_PlayerPos;
+	
+	};
+}
+
+
+#endif
