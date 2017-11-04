1672c217a719adcb4b4cab46edd7cb33f41f9d9a
Moto0116 <moto01160902@gmail.com>
2017-11-04 17:06:54 +0900

    経過時間管理クラス作成
    - 時間の描画
    - 時間管理
    - フォントの変更

--- Chemical/Chemical/Main/Application/Scene/GameScene/GameObjectManager/GameTimeManager/GameTimeManager.h
+++ Chemical/Chemical/Main/Application/Scene/GameScene/GameObjectManager/GameTimeManager/GameTimeManager.h
@@ -11,9 +11,14 @@
 //----------------------------------------------------------------------
 #include "ObjectManagerBase\ObjectBase\ObjectBase.h"
 
+#include "TaskManager\TaskBase\UpdateTask\UpdateTask.h"
+#include "TaskManager\TaskBase\DrawTask\DrawTask.h"
+
 
 namespace Game
 {
+	class TimeUI;
+
 	/*** ゲーム内時間管理クラス */
 	class TimeManager : public Lib::ObjectBase
 	{
@@ -38,6 +43,21 @@ namespace Game
 
 		/*** 描画処理 */
 		virtual void Draw();
+
+	private:
+		static const int m_FrameMax;
+		static const int m_SecondsMax;
+		static const int m_MinuteMax;
+
+		/*** 時間の更新処理 */
+		void TimeUpdate();
+
+		Lib::UpdateTask*	m_pUpdateTask;	//!< 更新タスクオブジェクト.
+		Lib::Draw2DTask*	m_pDrawTask;	//!< 描画タスクオブジェクト.
+		TimeUI*				m_pTimeUI;		//!< 時間UI.
+		int					m_Frame;		//!< 経過時間(フレーム).
+		int					m_Seconds;		//!< 経過時間(秒).
+		int					m_Minute;		//!< 経過時間(分).
 
 	};
 }
--- Chemical/Chemical/Main/Application/Scene/GameScene/GameObjectManager/GameTimeManager/GameTimeUI/GameTimeUI.h
+++ Chemical/Chemical/Main/Application/Scene/GameScene/GameObjectManager/GameTimeManager/GameTimeUI/GameTimeUI.h
@@ -9,13 +9,15 @@
 //----------------------------------------------------------------------
 // Include
 //----------------------------------------------------------------------
-#include "Object2DBase\Object2DBase.h"
+#include "ObjectManagerBase\ObjectBase\ObjectBase.h"
+
+#include "DirectX11\Font\Dx11Font.h"
 
 
 namespace Game
 {
 	/*** ゲームタイムUIクラス */
-	class TimeUI : public Object2DBase
+	class TimeUI : public Lib::ObjectBase
 	{
 	public:
 		/*** コンストラクタ */
@@ -38,6 +40,35 @@ namespace Game
 
 		/*** 描画処理 */
 		virtual void Draw();
+
+		/**
+		 * フレームの設定
+		 * @param[in] _frame 設定するフレーム
+		 */
+		void SetFrame(int _frame) { m_Frame = _frame; }
+
+		/**
+		 * 時間(秒)設定
+		 * @param[in] _minute 設定する時間(秒)
+		 */
+		void SetSeconds(int _seconds) { m_Seconds = _seconds; }
+
+		/**
+		 * 時間(分)の設定
+		 * @param[in] _minute 設定する時間(分)
+		 */
+		void SetMinute(int _minute) { m_Minute = _minute; }
+
+	private:
+		static const D3DXVECTOR2 m_DefaultPos;
+		static const D3DXVECTOR2 m_DefaultSize;
+
+		Lib::Dx11::Font* m_pFont;	//!< フォント描画オブジェクト.
+
+		int m_Frame;		//!< 経過時間(フレーム).
+		int m_MiliSeconds;	//!< 経過時間(ミリ秒).
+		int m_Seconds;		//!< 経過時間(秒).
+		int m_Minute;		//!< 経過時間(分).
 
 	};
 }
