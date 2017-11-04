4b10dc8ef132f2694a8877b31f3bda477b20cf41
Moto0116 <moto01160902@gmail.com>
2017-11-04 17:06:54 +0900

    経過時間管理クラス作成
    - 時間の描画
    - 時間管理
    - フォントの変更

--- Chemical/Chemical/Main/Application/Scene/GameScene/GameObjectManager/GameTimeManager/GameTimeUI/GameTimeUI.cpp
+++ Chemical/Chemical/Main/Application/Scene/GameScene/GameObjectManager/GameTimeManager/GameTimeUI/GameTimeUI.cpp
@@ -9,13 +9,26 @@
 //----------------------------------------------------------------------
 #include "GameTimeUI.h"
 
+#include "Debugger\Debugger.h"
+
 
 namespace Game
 {
 	//----------------------------------------------------------------------
+	// Static Private Variables
+	//----------------------------------------------------------------------
+	const D3DXVECTOR2 TimeUI::m_DefaultPos = D3DXVECTOR2(70, 650);
+	const D3DXVECTOR2 TimeUI::m_DefaultSize = D3DXVECTOR2(32, 64);
+
+
+	//----------------------------------------------------------------------
 	// Constructor	Destructor
 	//----------------------------------------------------------------------
-	TimeUI::TimeUI()
+	TimeUI::TimeUI() : 
+		m_Frame(0),
+		m_MiliSeconds(0),
+		m_Seconds(0),
+		m_Minute(0)
 	{
 	}
 
@@ -29,18 +42,41 @@ namespace Game
 	//----------------------------------------------------------------------
 	bool TimeUI::Initialize()
 	{
+		m_pFont = new Lib::Dx11::Font();
+		if (!m_pFont->Initialize(SINGLETON_INSTANCE(Lib::Dx11::GraphicsDevice)))
+		{
+			OutputErrorLog("フォントオブジェクトの初期化に失敗しました");
+			return false;
+		}
+
+		if (!m_pFont->CreateVertexBuffer(&m_DefaultSize))
+		{
+			OutputErrorLog("フォントオブジェクトの頂点生成に失敗しました");
+			return false;
+		}
+
+		m_pFont->SetAlignment(false);
+
 		return true;
 	}
 
 	void TimeUI::Finalize()
 	{
+		m_pFont->ReleaseVertexBuffer();
+		m_pFont->Finalize();
+		SafeDelete(m_pFont);
 	}
 
 	void TimeUI::Update()
 	{
+		m_MiliSeconds = m_Frame * 16;	// フレームをミリ秒に変換.
 	}
 
 	void TimeUI::Draw()
 	{
+		char Str[256];
+		sprintf_s(Str, "%d:%d:%d", m_Minute, m_Seconds, m_MiliSeconds / 10);
+
+		m_pFont->Draw(&m_DefaultPos, Str);
 	}
 }
